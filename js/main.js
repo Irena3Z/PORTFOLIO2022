f90093024fbd5d0a2fa941a2877cf5fef5fe9b05
Irena3Z <zbitkovski3@gmail.com>
2022-03-06 19:35:46 +0200

    added tittle section; added style; added information about me and my hobbies; added js files and jquery

--- js/main.js
+++ js/main.js
@@ -5,3 +5,12 @@ const nav = document.querySelector('.nav');
 toggleCollapse.onclick = function(event){
     nav.classList.toggle('collapse');
 }
+
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    autoplay:false,
+    autoplayTimeout:3000,
+    dots: false,
+    nav: true,
+    navText: [$('.owl-navigation .owl-nav-prev'),$('.owl-navigation .owl-nav-next')]
+});
